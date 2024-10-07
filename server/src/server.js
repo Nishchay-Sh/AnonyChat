8a490660d498cc34d30bb23afa179dabc617ce63
Nishchay sharma <nishchay340@gmail.com>
2024-10-07 10:17:16 +0530

    test

--- server/src/server.js
+++ server/src/server.js
@@ -8,50 +8,43 @@ const { config } = require('dotenv');
 const socketIo = require('socket.io');
 const cors = require('cors');
 
-// Load environment variables
 config();
 
-// Import MongoDB configuration
 
-// Initialize Express app
 const app = express();
 
-// Middlewares
-app.use(express.json()); // Parse JSON bodies
+app.use(express.json()); 
 app.use(cors());
 
-app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true })); 
 app.use(helmet({
     contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
     crossOriginOpenerPolicy: false,
     crossOriginResourcePolicy: false,
-})); // Security headers
-app.use(morgan('combined')); // HTTP request logger
+}));
 
-// Static files
+app.use(morgan('combined')); 
 app.use(express.static(join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, '../public/index.html'));
 });
 
-// Socket.io setup
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
 
 const io = socketIo(server);
-require('./utils/socket')(io); // Import socket.io handlers
+require('./utils/socket')(io); 
 
-// Import routes
 const routes = require('./routes/index');
 app.use('/api/v1', routes);
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
     serverSelectionTimeoutMS: 5000,
     ssl: true,
 })
     .then(() => console.log('Connected to MongoDB !!!'))
     .catch(err => console.error('MongoDB connection error:', err));
+
